a2262fbe00a97fb9df9c142543d6500ca02a7e2a
Katie <katiefaybehrmann@gmail.com>
2023-05-25 15:57:16 -0600

    Add login, signup forms and login hierarchy

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
 import { Route, Routes, Link } from "react-router-dom";
+import Login from "./Login";
+import React, { useEffect, useState } from "react";
+
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  if (!user) return <Login onLogin={setUser} />;
+
   return (
-    <div className="App">
-      <p>hold for content</p>
+    <div>
+      <p>navbar</p>
+      <p>hold for other views</p>
     </div>
   );
 }
---
+++ client/src/Login.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import LoginForm from "./LoginForm";
+import SignUpForm from "./SignUpForm";
+
+function Login({ onLogin }){
+    const [showLogin, setShowLogin] = useState(true);
+
+  return (
+    <Wrapper>
+      <h1>PlantPlan - Denver</h1>
+      {showLogin ? (
+        <>
+          <LoginForm onLogin={onLogin} />
+          <br />
+          <p>
+            Don't have an account? &nbsp;
+            <Button color="secondary" onClick={() => setShowLogin(false)}>
+              Sign Up
+            </Button>
+          </p>
+        </>
+      ) : (
+        <>
+          <SignUpForm onLogin={onLogin} />
+          <br />
+          <p>
+            Already have an account? &nbsp;
+            <Button color="secondary" onClick={() => setShowLogin(true)}>
+              Log In
+            </Button>
+          </p>
+        </>
+      )}
+    </Wrapper>
+  );
+}
+
+// const Logo = styled.h1`
+//   font-family: "Permanent Marker", cursive;
+//   font-size: 3rem;
+//   color: deeppink;
+//   margin: 8px 0 16px;
+// `;
+
+// const Wrapper = styled.section`
+//   max-width: 500px;
+//   margin: 40px auto;
+//   padding: 16px;
+// `;
+
+// const Divider = styled.hr`
+//   border: none;
+//   border-bottom: 1px solid #ccc;
+//   margin: 16px 0;
+// `;
+
+export default Login;
