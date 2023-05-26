2c4c6b51f7fd85c854e3d6de57dbf99cadadedf2
Katie <katiefaybehrmann@gmail.com>
2023-05-25 19:11:33 -0600

    Clean up messy npm packages

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
@@ -22,9 +22,9 @@ const GlobalStyle = createGlobalStyle`
 
 
 ReactDOM.render(
-  <BrowserRouter>
-    <GlobalStyle />
+  <Router>
+    {/* <GlobalStyle /> */}
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById("root")
 );
